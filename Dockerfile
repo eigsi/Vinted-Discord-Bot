55f0145d519502fe3f95a665dda143560c18c402
izana <126054825+eigsi@users.noreply.github.com>
2024-05-27 21:06:34 +0200

    ajout d'un .env pour les variables d'environnement

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,10 @@ LABEL authors="antoine"
 WORKDIR /app
 COPY package*.json app.js ./
 RUN npm install
+COPY . .
 EXPOSE 3000
-
 # Point de montage
 VOLUME /app/data
 
-CMD ["sh","-c", "echo 'Pitié faites que ça marche' > /app/data/test.txt && npm start"]
-
+CMD ["sh","-c", "echo 'Pitié faites que ça marche...' > /app/data/test.txt && npm start"]
 
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get ('/', (req, res ) => res.send('Hello World!'));
-app.listen (3000, () => console.log('Server is running on port 3000'));
+const port = process.env.PORT || 3000; //variable par défaut au cas ou
+const message = process.env.MESSAGE || 'Hello World!';
+
+app.get ('/', (req, res ) => res.send(message));
+app.listen (port, () => console.log('Server is running on port *{port}'));
